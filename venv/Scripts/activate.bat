e43b956acbae79c9c560e68c00741202c22f9820
Jakob-98 <jakobserlier@gmail.com>
2019-11-11 16:48:03 -0500

    added keys - fixed bot

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=C:\Users\jakob\PycharmProjects\TwiBot\venv"
+set "VIRTUAL_ENV=C:\Users\jakob\PycharmProjects\tbot\venv"
 
 if not defined PROMPT (
     set "PROMPT=$P$G"
--- venv/twitterbot.py
+++ venv/twitterbot.py
@@ -2,16 +2,22 @@ import tweepy
 import os
 
 
-
 #import token from token.txt file in same directory
-token_file = os.path.join(path, "token.txt")
+token_file = os.path.join('../', "token.txt")
 
-with open(token_file, 'rb') as f:
-    token = f.read().replace('\n', '')
+tokens = open(token_file,'r')
+
+for line in tokens:
+    keys = line.split(';')
+    CONSUMER_KEY = keys[0]
+    CONSUMER_SECRET = keys[1]
+    ACCESS_TOKEN = keys[2]
+    ACCESS_TOKEN_SECRET = keys[3]
+
 
 # Authenticate to Twitter
-auth = tweepy.OAuthHandler("CONSUMER_KEY", "CONSUMER_SECRET")
-auth.set_access_token("ACCESS_TOKEN", "ACCESS_TOKEN_SECRET")
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 
 # Create API object
 api = tweepy.API(auth)
